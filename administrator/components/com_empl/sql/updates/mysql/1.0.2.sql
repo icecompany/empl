67cc7253c359d4c78deeca4dfdb28250a65806c1
asharikov@icecompany.org <44duP_yx579>
2019-12-17 13:22:01 +0300

    Version 1.0.2

--- administrator/components/com_empl/sql/updates/mysql/1.0.2.sql
+++ administrator/components/com_empl/sql/updates/mysql/1.0.2.sql
@@ -1,6 +1,6 @@
 alter table `#__empl_employers` modify metroID int default null null;
 
-alter table `#__empl_employers` alter column address set default null;
+alter table `#__empl_employers` modify address varbinary(255) default null null;
 
 alter table `#__empl_employers` modify night tinyint default null null comment 'Готовность работы в ночное время';
 
