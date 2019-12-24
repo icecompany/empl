fa92f429db1f3fd11441f47d428eb1d9008c90b4
asharikov@icecompany.org <44duP_yx579>
2019-12-24 12:01:46 +0300

    Schedule table

---
+++ administrator/components/com_empl/sql/updates/mysql/1.1.1.sql
@@ -0,0 +1,27 @@
+create table `#__empl_schedule`
+(
+    id int unsigned auto_increment,
+    workID int unsigned not null,
+    functionID int unsigned not null,
+    placeID int unsigned not null,
+    curator tinyint default 0 not null,
+    start_time timestamp default current_timestamp not null,
+    end_time timestamp default current_timestamp not null,
+    comment text default null null,
+    constraint `#__empl_schedule_pk`
+        primary key (id),
+    constraint `#__empl_schedule_#__empl_functions_id_fk`
+        foreign key (functionID) references `#__empl_functions` (id)
+            on update cascade on delete cascade,
+    constraint `#__empl_schedule_#__empl_places_id_fk`
+        foreign key (placeID) references `#__empl_places` (id)
+            on update cascade on delete cascade,
+    constraint `#__empl_schedule_#__empl_work_id_fk`
+        foreign key (workID) references `#__empl_work` (id)
+            on update cascade on delete cascade
+)
+    comment 'Расписание работы волонтёров';
+
+create index `#__empl_schedule_curator_index`
+    on `#__empl_schedule` (curator);
+
